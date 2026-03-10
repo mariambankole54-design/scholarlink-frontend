c27dbc4b59ff1814ce7cdd8a32192c242e272ea3
Mariam Bankole <mariambankole54@gmail.com>
2026-03-10 09:17:02 +0000

    deployed

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../App.css';
+
 
 const UserDashboard = () => {
   const [universities, setUniversities] = useState([]);
